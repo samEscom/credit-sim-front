107be3cacc384f80e9bb1d791b8b9e9e8d844c80
SamChavez <sa5m.escom@gmail.com>
2026-02-06 20:03:07 -0600

    feat: app skeleton

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Container, Text, Title } from '@mantine/core'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Container size="sm" py="xl">
+        <Title order={2}>CreditSim</Title>
+        <Text c="dimmed" mt="xs">
+          Simulador de crédito
+        </Text>
+      </Container>  <Container>
+        <Title>Credit Sim</Title>
+        <Text>Credit Sim</Text>
+      </Container>
+    </>
+  )
+}
+
+export default App
